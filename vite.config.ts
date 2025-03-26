8d496431e2a9651e9ffe2bbb8bebde92d815928f
yaotutu <957810668@qq.com>
2025-03-26 18:01:01 +0800

    新增 Tailwind CSS 支持：更新依赖项以包含 Tailwind CSS 和相关插件，配置 Vite 以使用 Tailwind CSS，添加 Tailwind CSS 导入到主样式文件中

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
+import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
